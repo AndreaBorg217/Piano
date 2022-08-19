e81ae54795d4916ea533233c08a60a79bfca09cf
Andrea Borg <andreaborg217@gmail.com>
2022-08-19 13:40:46 +0200

    initialised project

---
+++ App.js
@@ -0,0 +1,36 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React, {useState, useEffect, useRef} from 'react';
+ import {View, StyleSheet, Text, ImageBackground} from 'react-native';
+ 
+ 
+ 
+ const App = () => {
+
+ 
+   return (
+     <View style={styles.container}>
+     </View>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     width: '100%',
+     height: '100%',
+     flex: 1,
+     backgroundColor: '#123',
+     alignItems: 'center',
+     justifyContent: 'center'
+   },
+ });
+ 
+ export default App;
+ 
